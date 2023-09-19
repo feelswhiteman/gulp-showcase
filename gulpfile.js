22f9a4e028c1b0c7a6657dddec3aaa57244e3b20
feelswhiteman <feelswhiteman@gmail.com>
2023-09-19 19:32:16 +0300

    Server task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
+import { plugins } from "./gulp/config/plugins.js";
+
 import { copy } from "./gulp/tasks/copy.js";
 import { clean } from "./gulp/tasks/clean.js";
 import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js"
 
 global.app = {
-    path: path,
-    gulp: gulp,
+    path,
+    gulp,
+    plugins,
 };
 
 const mainTasks = gulp.parallel(copy, html);
-const devTasks = gulp.series(clean, mainTasks, watch);
+const devTasks = gulp.series(clean, mainTasks, gulp.parallel(watch, server));
 
 function watch() {
     gulp.watch(path.watch.files, devTasks);
