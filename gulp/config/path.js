742098105e45613f6ecfe4fe688bf21536416d6b
feelswhiteman <feelswhiteman@gmail.com>
2023-10-02 14:10:25 +0300

    Image optimization

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,18 +6,25 @@ const srcFolder = "./src";
 
 export const path = {
     build: {
+        js: `${buildFolder}/js/`,
         css: `${buildFolder}/css/`,
         html: `${buildFolder}/`,
+        images: `${buildFolder}/img/`,
         files: `${buildFolder}/files/`,
     },
     src: {
+        js: `${srcFolder}/js/app.js`,
         scss: `${srcFolder}/scss/style.scss`,
         html: `${srcFolder}/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
         files: `${srcFolder}/files/**/*`,
     },
     watch: {
+        js: `${srcFolder}/js/**/*.js`,
         scss: `${srcFolder}/scss/**/*.scss`,
         html: `${srcFolder}/**/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
         files: `${srcFolder}/files/**/*`,
     },
     clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ import { copy } from "./gulp/tasks/copy.js";
 import { clean } from "./gulp/tasks/clean.js";
 import { html } from "./gulp/tasks/html.js";
 import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
 import { server } from "./gulp/tasks/server.js";
 
 global.app = {
@@ -18,9 +20,11 @@ function watcher() {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss);
+const mainTasks = gulp.parallel(copy, html, scss, js, images);
 const devTasks = gulp.series(clean, mainTasks, gulp.parallel(server, watcher));
 
 gulp.task("default", devTasks);
